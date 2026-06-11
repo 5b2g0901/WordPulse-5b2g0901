99d1b223adfda8f4c478f42f2cfc90a16d24edb5
5b2g0901 <5b2g0901@stust.edu.tw>
2026-06-11 10:47:57 +0800

    02

--- 5b2g0901/vite.config.js
+++ 5b2g0901/vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/WORDPULSE-5B2G0901-1/'
 })
