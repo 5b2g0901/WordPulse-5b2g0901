d9279867d096175f0da6af461be5026858f88f5a
5b2g0901 <5b2g0901@stust.edu.tw>
2026-06-18 21:09:28 +0800

    fix: update wordpulse image path and remove syllabus

--- 5b2g0901/vite.config.js
+++ 5b2g0901/vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/WordPulse-5b2g0901/'
+  base: "/WordPulse-5b2g0901-1/", // Hoặc tên kho lưu trữ WordPulse chính xác của bạn trên GitHub
 })
